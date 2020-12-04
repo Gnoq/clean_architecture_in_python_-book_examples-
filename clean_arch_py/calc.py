9d4f71d238b5ef7372207ffcda82422f9b64468e
Gnoq <emi.cugno@gmail.com>
2020-12-04 20:46:20 -0300

    Add subtraction and multiplication test cases

--- clean_arch_py/calc.py
+++ clean_arch_py/calc.py
@@ -1,3 +1,15 @@
+
+from functools import reduce
+
+
 class Calc:
-    def add(self, a, b):
-        return 9
+    def add(self, *args):
+        return sum(args)
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, *args):
+        def mul2(a, b):
+            return a * b
+        return reduce(mul2, args)
